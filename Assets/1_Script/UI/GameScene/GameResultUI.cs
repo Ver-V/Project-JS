7d14465fc78f61a2d369d98116640a6205913764
GinipigKR <ginipigeu23@gmail.com>
2026-06-06 16:13:37 +0900

    fix : 네임스페이스 추가

--- Assets/1_Script/UI/GameScene/GameResultUI.cs
+++ Assets/1_Script/UI/GameScene/GameResultUI.cs
@@ -1,57 +1,60 @@
-using ProjectJS.UI.GameScene;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameResultUI : MonoBehaviour
+namespace ProjectJS.UI.GameScene
 {
-    private const string _bossClearDescMessage = "파티가 보스 토벌에 성공했습니다.";
-    private const string _bossFailDescMessage = "파티가 전멸했습니다.";
-    private const string _clientWaitMessage = "호스트의 선택을 기다리는 중...";
-
-    [SerializeField] private TMP_Text bossClearMainText;
-    [SerializeField] private TMP_Text bossClearSubText;
-    [SerializeField] private TMP_Text bossClearDescText;
-
-    [SerializeField] private TMP_Text clientWaitText;
-    [SerializeField] private Button retryButton;
-    [SerializeField] private Button lobbyButton;
-
-    private void Start()
+    public class GameResultUI : MonoBehaviour
     {
-        //retryButton.onClick.AddListener();
-        lobbyButton.onClick.AddListener(GameSceneUI.Instance.ReturnToLobby);
-    }
+        private const string _bossClearDescMessage = "파티가 보스 토벌에 성공했습니다.";
+        private const string _bossFailDescMessage = "파티가 전멸했습니다.";
+        private const string _clientWaitMessage = "호스트의 선택을 기다리는 중...";
 
-    public void ShowGameResult(bool isClear, bool isHost)
-    {
-        if (isClear)
+        [SerializeField] private TMP_Text bossClearMainText;
+        [SerializeField] private TMP_Text bossClearSubText;
+        [SerializeField] private TMP_Text bossClearDescText;
+
+        [SerializeField] private TMP_Text clientWaitText;
+        [SerializeField] private Button retryButton;
+        [SerializeField] private Button lobbyButton;
+
+        private void Start()
         {
-            bossClearMainText.text = "Boss Clear";
-            bossClearSubText.text = "처치 성공";
-            bossClearDescText.text = _bossClearDescMessage;
-        }
-        else
-        {
-            bossClearMainText.text = "Boss Failed";
-            bossClearSubText.text = "처치 실패";
-            bossClearDescText.text = _bossFailDescMessage;
+            //retryButton.onClick.AddListener();
+            lobbyButton.onClick.AddListener(GameSceneUI.Instance.ReturnToLobby);
         }
 
-        clientWaitText.gameObject.SetActive(!isHost);
-        retryButton.gameObject.SetActive(isHost);
-        lobbyButton.gameObject.SetActive(isHost);
-
-        if (!isHost)
+        public void ShowGameResult(bool isClear, bool isHost)
         {
-            clientWaitText.text = _clientWaitMessage;
+            if (isClear)
+            {
+                bossClearMainText.text = "Boss Clear";
+                bossClearSubText.text = "처치 성공";
+                bossClearDescText.text = _bossClearDescMessage;
+            }
+            else
+            {
+                bossClearMainText.text = "Boss Failed";
+                bossClearSubText.text = "처치 실패";
+                bossClearDescText.text = _bossFailDescMessage;
+            }
+
+            clientWaitText.gameObject.SetActive(!isHost);
+            retryButton.gameObject.SetActive(isHost);
+            lobbyButton.gameObject.SetActive(isHost);
+
+            if (!isHost)
+            {
+                clientWaitText.text = _clientWaitMessage;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            lobbyButton.onClick.RemoveAllListeners();
+            retryButton.onClick.RemoveAllListeners();
         }
     }
 
-    private void OnDestroy()
-    {
-        lobbyButton.onClick.RemoveAllListeners();
-        retryButton.onClick.RemoveAllListeners();
-    }
 }
+
